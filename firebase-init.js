75a7cb7c5695651c9f9fcb9ab3103243041ce35e
Tacticizm <Tacticizm@users.noreply.github.com>
2026-05-24 02:50:33 -0400

    Fix API key (O vs 0 typo)

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCmPJF0Ykhhduxjrmgf09kJGW8KMAeHdn8",
+    apiKey: "AIzaSyCmPJF0YkhhduxjrmgfO9kJGW8KMAeHdn8",
     authDomain: "tactscasino.firebaseapp.com",
+    databaseURL: "https://tactscasino-default-rtdb.firebaseio.com",
     projectId: "tactscasino",
     storageBucket: "tactscasino.firebasestorage.app",
     messagingSenderId: "672146244987",
-    appId: "1:672146244987:web:b5142c80938348b7b269de"
+    appId: "1:672146244987:web:b5142c80938348b7b269de",
+    measurementId: "G-0J9HH9QRQK"
 };
 
 firebase.initializeApp(firebaseConfig);
